696e4c623392e8c8f4713c3c9330f3e689c92323
Rodrigo Campos <70313745+rdgpcampos@users.noreply.github.com>
2022-02-02 11:45:48 +0900

    Update database_objects.py

--- database_objects.py
+++ database_objects.py
@@ -74,12 +74,6 @@ def insert_order(session,client_id,status):
     
 def connect_to_db():
     engine = create_engine("postgresql+psycopg2://postgres:postgres@db:5432/postgres")
-    
-    if not database_exists(engine.url): # Checks for the first time  
-        create_database(engine.url)     # Create new DB    
-        print("New Database Created"+database_exists(engine.url)) # Verifies if database is there or not.
-    else:
-        print("Database Already Exists")
 
     metadata = MetaData(engine)
     
